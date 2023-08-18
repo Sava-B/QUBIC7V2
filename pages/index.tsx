cc8dc5ebadf736578dbac8d4db59055859ffce5d
Sava-B <savab2164@gmail.com>
2023-08-17 21:23:47 -0600

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
+import { Typography, Container, Box } from "@mui/material";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <Container>
+      <h1>Hello, Next.js!</h1>
+      <Typography>Test</Typography>
+      <Box sx={{border: 'primary.main'}}></Box>
+    </Container>
+    
+  )
+
 }
