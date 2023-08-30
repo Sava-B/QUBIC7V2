5ec7c84ae3c5c020ecfa032a00679316c9eb987a
Sava <savab2164@gmail.com>
2023-08-30 17:59:41 -0600

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import { Typography, Container, Box } from "@mui/material";
+import { Typography, Container, Box, Paper } from "@mui/material";
 
 export default function Page() {
   return (
-    <Container>
+    <Container disableGutters maxWidth={false} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
       <h1>Hello, Next.js!</h1>
       <Typography>Test</Typography>
-      <Box sx={{border: 'primary.main'}}></Box>
+      <Box sx={{border: 'primary.main'}}>
+        <Paper elevation={20} sx={{height: '30vh', width: '80vw'}}>
+            Something
+        </Paper>
+      </Box>
     </Container>
     
   )
